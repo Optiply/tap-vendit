d9e9b17f9f0e557e4973b5bbc683d9c56b0d813b
Xavi <117915152+XAbade@users.noreply.github.com>
2026-04-30 10:26:28 +0100

    Delete tap_vendit directory

--- tap_vendit/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Tap for Vendit."""
-
-from tap_vendit.tap import TapVendit
-
-__all__ = ["TapVendit"]
--- tap_vendit/__main__.py
+++
@@ -1,7 +0,0 @@
-"""Vendit entry point."""
-
-from __future__ import annotations
-
-from tap_vendit.tap import TapVendit
-
-TapVendit.cli()
